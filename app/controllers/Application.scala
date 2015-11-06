1079042c2f9986031e67e03379333fa932ca8b0e
Uchiko <memememomo@gmail.com>
2015-11-06 20:23:53 +0900

    fixed

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,20 @@
 package controllers
 
+import java.io.File
+
 import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  def index = Action {
-    Ok("Hello world")
+  def download(name: String) = Action {
+    var file = new File("public/%s".format(name))
+    if (file.exists()) {
+      Ok.sendFile(
+        content = file,
+        inline = true
+      )
+    } else {
+      NotFound
+    }
   }
 }
